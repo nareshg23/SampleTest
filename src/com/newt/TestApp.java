88d8ce1146b1c2e072e8dac36769b0307f540b5f
nareshg23 <nareshg23@users.noreply.github.com>
2017-03-16 16:43:04 +0530

    #JIRA#API#UI#

--- src/com/newt/TestApp.java
+++ src/com/newt/TestApp.java
@@ -2,10 +2,7 @@ package com.newt;
 
 public class TestApp {
 	public static void main(String[] args) {
-		System.out.println("Checking the Custom plugin for Jenkins");
-		
-		
-		
+		System.out.println("Checking the Custom plugin for Jenkins");	
 		
 	}
 
